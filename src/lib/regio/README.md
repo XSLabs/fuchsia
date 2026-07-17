fa1b6e34dea14bd3a5b822ce03e4b5ce54bdf28c
Joshua Seaton <joshuaseaton@google.com>
2026-07-17 16:41:40 -0700

    [regio] Introduce core Register abstraction
    `Register` represents a structured register layout, and its access
    interface and permissions. It is the core abstraction of the crate.
    
    As a meaningful specialization of this type (so that we can propery
    exercise/test it at all in this change), we introduce MMIO support here
    as well.
    
    DISABLE_SPELLCHECKER  # It keeps firing for "regio"
    
    Bug: 525077555
    Change-Id: I6e75ef3584fe88bb207513eed148cc6ba60276d5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1687576
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>
    Reviewed-by: Ethan Williams <ethanws@google.com>

--- src/lib/regio/README.md
+++ src/lib/regio/README.md
@@ -3,3 +3,99 @@
 `regio` ("reh-jee-o") is a general, extensible framework for register I/O. It is
 agnostic of the choice of layout type, and aims to support varied notions of
 'register' across common CPU architectures and hardware.
+
+## `Register`
+
+`Register` represents an abstract register tied to a specific layout, access
+permissions, and an 'I/O backend' defining how it is accessed. Its API admits
+the usual access methods when its permissions and backend permit them.
+
+Conventions:
+
+- It is expected that hardware-specific notions of register define themselves as
+  a type alias or wrapper of `Register` and implement their own specialized
+  constructors.
+
+- When register instances are `const`-constructible, they ought to be `const`s
+  and carry the same name as they are documented in architectural manuals. They
+  are usually documented in screaming snake case already, so this makes for a
+  nice, readable coincidence.
+
+## `Value`
+
+- `Value` represents a prospective value for a register. It is a thin wrapper of
+  a layout value that retains its knowledge of the I/O backend, admitting
+  mirrored writeback methods when the access permissions and backend permit
+  them. Instances are returned by `Register::read()` and may also be minted via
+  `Register::into_value()`.
+
+## MMIO
+
+The core MMIO types are `Offset`, `Mmio`, `MmioPtr`, and `MmioBank`. `Mmio` is
+an alias of `Register` representing an MMIO register. Often MMIO addresses are
+not statically known; in this case, we port the above `const` `Register`
+convention over to defining the offset descriptors - which are usually
+statically known - to be `const` instances bearing the documented register
+names.
+
+The following is example logic for driving a SiFive UART.
+
+```rust
+use core::ptr;
+
+use bitrs::layout;
+use regio::{MmioBank, MmioPtr, Offset, RwSafe};
+
+layout!({
+    struct TransmitDataRegister(u32);
+    {
+        let full @ 31; // Whether the TX FIFO is full.
+        let __ @ 30..8;
+        let data @ 7..0;
+    }
+});
+
+const TXDATA: Offset<TransmitDataRegister, RwSafe> = Offset::new(0);
+
+layout!({
+    struct ReceiveDataRegister(u32);
+    {
+        let empty @ 31; // Whether the RX FIFO is empty.
+        let __ @ 30..8;
+        let data @ 7..0;
+    }
+});
+
+const RXDATA: Offset<ReceiveDataRegister, RwSafe> = Offset::new(4);
+
+layout!({
+    struct InterruptEnableRegister(u32);
+    {
+        let _ @ 31..2;
+        let rxwm @ 1; // Receive watermark interrupt enable
+        let txwm @ 0; // Transmit watermark interrupt enable
+    }
+});
+
+const IE: Offset<InterruptEnableRegister, RwSafe> = Offset::new(0x10);
+
+// ...
+
+const BANK_SIZE = 0x1c; // The highest documented offset is 0x18.
+
+// Could be made const in this case, but in practice it may not be.
+let uart_base = unsafe {
+    MmioPtr::<u32, RwSafe>::new(ptr::with_exposed_provenance_mut(0x1001_3000))
+};
+let uart = MmioBank::new(uart_base, BANK_SIZE);
+
+// Can access the MmioBank to get an Mmio.
+unsafe { uart.at(IE) }.write(0u32.into());  // Disable all interrupts.
+
+// Can construct a Value directly for write-back.
+let data = TransmitDataRegister::from(0).set_data(0xab);
+unsafe { uart.at(TXDATA) }.into_value(data).write();
+
+// Simple reads can fluidly chain from MmioPtr -> Mmio -> Layout.
+let _: u8 = unsafe { uart.at(RXDATA) }.read().data();
+```
---
+++ src/lib/regio/src/lib.rs
@@ -0,0 +1,416 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![no_std]
+
+mod mmio;
+pub use mmio::{Mmio, MmioBank, MmioPtr, Offset};
+
+use core::marker::PhantomData;
+use core::ops::{Deref, DerefMut};
+
+mod private {
+    pub trait Sealed {}
+}
+
+/// Base marker for register access permissions.
+///
+/// This trait is sealed and cannot be implemented outside of this crate,
+/// ensuring that permissions cannot be forged.
+pub trait Accessible: private::Sealed {}
+
+/// A tag for register read access.
+#[derive(Clone, Copy, Debug)]
+pub enum Read {}
+
+/// A tag for disallowed register read access.
+#[derive(Clone, Copy, Debug)]
+pub enum NoRead {}
+
+/// A tag for safe register write access: this is when writes to the register
+/// are statically known to be sound (e.g., in the cases of MMIO that lights up
+/// an LED or a performance counter system register).
+#[derive(Clone, Copy, Debug)]
+pub enum SafeWrite {}
+
+/// A tag for unsafe register write access: this is when writes to the register
+/// may contextually violate the memory model (e.g., system register that
+/// toggles the MMU or in modeling a page table entry as a register). In such
+/// cases, the user must attest to any given write being sound.
+#[derive(Clone, Copy, Debug)]
+pub enum UnsafeWrite {}
+
+/// A tag for disallowed register write access.
+#[derive(Clone, Copy, Debug)]
+pub enum NoWrite {}
+
+impl private::Sealed for (Read, NoWrite) {}
+impl private::Sealed for (Read, SafeWrite) {}
+impl private::Sealed for (Read, UnsafeWrite) {}
+impl private::Sealed for (NoRead, SafeWrite) {}
+impl private::Sealed for (NoRead, UnsafeWrite) {}
+
+impl Accessible for (Read, NoWrite) {}
+impl Accessible for (Read, SafeWrite) {}
+impl Accessible for (Read, UnsafeWrite) {}
+impl Accessible for (NoRead, SafeWrite) {}
+impl Accessible for (NoRead, UnsafeWrite) {}
+
+/// Marker for readable register access.
+pub trait Readable: Accessible {}
+impl Readable for (Read, NoWrite) {}
+impl Readable for (Read, SafeWrite) {}
+impl Readable for (Read, UnsafeWrite) {}
+
+/// Marker for writable register access.
+pub trait Writable: Accessible {}
+impl Writable for (Read, SafeWrite) {}
+impl Writable for (Read, UnsafeWrite) {}
+impl Writable for (NoRead, SafeWrite) {}
+impl Writable for (NoRead, UnsafeWrite) {}
+
+// Aliases for brevity.
+
+/// A tag for read-only register access.
+pub type Ro = (Read, NoWrite);
+
+/// A tag for read + safe-write register access.
+pub type RwSafe = (Read, SafeWrite);
+
+/// A tag for read + unsafe-write register access.
+pub type RwUnsafe = (Read, UnsafeWrite);
+
+/// A tag for safe-write-only register access.
+pub type WoSafe = (NoRead, SafeWrite);
+
+/// A tag for unsafe-write-only register access.
+pub type WoUnsafe = (NoRead, UnsafeWrite);
+
+/// Expresses how one set of access permissions can imply a narrower set. For
+/// example, read-writable should imply readable.
+pub trait AccessRestrictsTo<Access: Accessible>: Accessible {}
+
+// Any set of permissions restricts to itself.
+impl<Access: Accessible> AccessRestrictsTo<Access> for Access {}
+
+impl AccessRestrictsTo<(Read, NoWrite)> for (Read, SafeWrite) {}
+impl AccessRestrictsTo<(Read, NoWrite)> for (Read, UnsafeWrite) {}
+
+impl AccessRestrictsTo<(Read, UnsafeWrite)> for (Read, SafeWrite) {}
+
+impl AccessRestrictsTo<(NoRead, SafeWrite)> for (Read, SafeWrite) {}
+impl AccessRestrictsTo<(NoRead, UnsafeWrite)> for (Read, UnsafeWrite) {}
+
+impl AccessRestrictsTo<(NoRead, UnsafeWrite)> for (NoRead, SafeWrite) {}
+impl AccessRestrictsTo<(NoRead, UnsafeWrite)> for (Read, SafeWrite) {}
+
+/// A convenience supertrait for the traits expected of layout types over a
+/// specific base type. This is not intended to be implemented explicitly, only
+/// through its blanket implementation.
+pub trait LayoutOver<Base>: Copy + From<Base> + Into<Base> {}
+
+impl<Layout, Base> LayoutOver<Base> for Layout where Layout: Copy + From<Base> + Into<Base> {}
+
+/// Represents an abstracted means of register access.
+pub trait IoHandle {
+    /// The underlying base type of the register (assumed integral in
+    /// practice).
+    type Base: Copy;
+}
+
+/// Represents an abstracted means of register reads.
+pub trait ReadHandle: IoHandle {
+    /// Performs the read.
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee that the implementation-specific
+    /// preconditions for a sound read are met. For example, in the case of a
+    /// pointer, that the instance is valid, properly aligned, and pointing to
+    /// initialized memory.
+    unsafe fn read_raw(&self) -> Self::Base;
+}
+
+/// Represents an abstracted means of register writes.
+pub trait WriteHandle: IoHandle {
+    /// Performs the write of the provided value.
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee...
+    ///
+    /// * that the value-agnostic, implementation-specific preconditions for a
+    ///   sound write are met;
+    ///
+    /// * and further that the particular value will not cause undefined
+    ///   behaviour when written (in an implementation-specific way).
+    ///
+    /// For example, in the case of a pointer to an MMIO address, the
+    /// conditions would be that the pointer instance is valid and aligned, and
+    /// that the particular value to write does not misconfigure hardware in a
+    /// way that leads to undefined behavior (such as configuring DMA to
+    /// overwrite arbitrary memory).
+    unsafe fn write_raw(&self, value: Self::Base);
+}
+
+/// `Register` represents a structured register layout, and its access
+/// interface and permissions. This is the core abstraction of the crate.
+///
+/// One may convert a register instance into a [`Value`], which holds a
+/// prospective value for the register along with methods to actually write it
+/// back. This can be done via [`Register::read`] or [`Value::new`]. Values may
+/// also be directly written to the register via [`Register::write`].
+///
+/// If `Access` expresses unsafe-writability, then all write methods are marked
+/// as unsafe.
+#[derive(Clone, Copy, Debug)]
+pub struct Register<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    io: Io,
+    _marker: PhantomData<(Layout, Access)>,
+}
+
+impl<Layout, Access, Io> Register<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    /// Constructs a new register from an I/O handle.
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee...
+    ///
+    /// * that the provided I/O handle meets the implementation-specific
+    ///   access safety preconditions for the duration of the lifetimes of the
+    ///   register instance and any copies of it (e.g., in the case of an MMIO
+    ///   pointer, that the pointer is aligned and the memory it points to
+    ///   remains mapped for those lifetimes);
+    ///
+    /// * and that `Access` correctly models the safe nature of access in this
+    ///   context.
+    pub const unsafe fn from_io(io: Io) -> Self {
+        Self { io, _marker: PhantomData }
+    }
+
+    pub const fn io(&self) -> &Io {
+        &self.io
+    }
+
+    // TODO(https://github.com/rust-lang/rust/issues/73255): Make this const
+    // when ergonomically easy. Ditto for the into_*() methods below.
+    pub fn into_io(self) -> Io {
+        self.io
+    }
+
+    /// Constructs a particular value instance tied to this register.
+    pub fn into_value(self, value: Layout) -> Value<Layout, Access, Io> {
+        Value::new(self, value)
+    }
+
+    /// Reads from the register, if the backend and register permit it.
+    #[inline]
+    pub fn read(self) -> Value<Layout, Access, Io>
+    where
+        Access: Readable,
+        Io: ReadHandle,
+    {
+        let value: Layout = self.read_impl();
+        self.into_value(value)
+    }
+
+    // Read subroutine consolidating the safety justification of the access.
+    #[inline(always)]
+    fn read_impl(&self) -> Layout
+    where
+        Access: Readable,
+        Io: ReadHandle,
+    {
+        // Safety: The I/O handle was attested as meeting the handle-specific
+        // preconditions for reading for the duration of our lifetime.
+        unsafe { self.io.read_raw().into() }
+    }
+
+    // Write subroutine consolidating the handle-specific safety justification
+    // of the access.
+    //
+    // # Safety
+    //
+    // The caller must guarantee that the particular value will not cause
+    // undefined behaviour when written.
+    #[inline(always)]
+    unsafe fn write_impl(&self, value: Layout)
+    where
+        Access: Writable,
+        Io: WriteHandle,
+    {
+        // Safety: The I/O handle was attested as meeting the
+        // handle-specific preconditions for writing for our lifetime; the
+        // value-specific preconditions are left to the caller to justify in
+        // the case of unsafe-writability. Moreover, the caller attested to the
+        // safeness/unsafeness of the write access in general.
+        unsafe { self.io.write_raw(value.into()) }
+    }
+}
+
+/// Represents a prospective value for a given register, a "shadow" of sorts. It
+/// admits convenient write-back methods, and crucially the value is not
+/// actually committed until the caller calls `write()`.
+///
+/// Value implements [`core::ops::Deref`] and [`core::ops::DerefMut`] for access
+/// to the underlying value.
+#[derive(Clone, Copy, Debug)]
+pub struct Value<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    value: Layout,
+    register: Register<Layout, Access, Io>,
+}
+
+impl<Layout, Access, Io> Value<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    /// Returns a prospective value for the provided register.
+    pub const fn new(register: Register<Layout, Access, Io>, value: Layout) -> Self {
+        Self { value, register }
+    }
+
+    pub fn into_register(self) -> Register<Layout, Access, Io> {
+        self.register
+    }
+
+    pub const fn io(&self) -> &Io {
+        self.register.io()
+    }
+
+    pub fn into_io(self) -> Io {
+        self.register.into_io()
+    }
+
+    /// Returns the underlying value.
+    pub const fn get(&self) -> Layout {
+        self.value
+    }
+}
+
+impl<Layout, Access, Io> Deref for Value<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    type Target = Layout;
+
+    fn deref(&self) -> &Layout {
+        &self.value
+    }
+}
+
+impl<Layout, Access, Io> DerefMut for Value<Layout, Access, Io>
+where
+    Io: IoHandle,
+    Layout: LayoutOver<<Io as IoHandle>::Base>,
+    Access: Accessible,
+{
+    fn deref_mut(&mut self) -> &mut Layout {
+        &mut self.value
+    }
+}
+
+// This will be used to stamp out write-related methods differing only in
+// safety, across the disjoint cases of safe- and unsafe-writability
+macro_rules! impl_writable {
+    ($write_kind:ident) => {
+        impl_writable!(@impl $write_kind);
+    };
+    ($write_kind:ident, unsafe) => {
+        impl_writable!(
+            @impl $write_kind,
+            unsafe,
+            ///
+            /// # Safety
+            ///
+            /// The caller must guarantee that the write does not result in
+            /// undefined behaviour.
+        );
+    };
+    (
+        @impl $write_kind:ident
+        $(, $unsafe:ident )?
+        $(, $(#[$safety_doc:meta])* )?
+    ) => {
+        impl<Layout, R, Io> Register<Layout, (R, $write_kind), Io>
+        where
+            Io: WriteHandle,
+            Layout: LayoutOver<<Io as IoHandle>::Base>,
+            (R, $write_kind): Writable,
+        {
+            /// Writes to the register, if the backend and register permit it.
+            $($(#[$safety_doc])*)?
+            #[inline]
+            pub $($unsafe)? fn write(&self, value: Layout) {
+                // Safety: In the case of unsafe-writability, this method is
+                // unsafe and the caller themselves must provide the
+                // justification.
+                #[allow(unused_unsafe)]
+                unsafe { self.write_impl(value) }
+            }
+
+            /// Modifies the contents of the register, if the register and backend admit
+            /// both reads and writes, returning whatever state the modification
+            /// callback wanted to forward.
+            $($(#[$safety_doc])*)?
+            #[inline]
+            pub $($unsafe)? fn modify<ModifyFn, Ret>(&self, cb: ModifyFn) -> Ret
+            where
+                (R, $write_kind): Readable + Writable,
+                Io: ReadHandle,
+                ModifyFn: FnOnce(&mut Layout) -> Ret,
+            {
+                let mut value = self.read_impl();
+
+                let ret = cb(&mut value);
+
+                // Safety: In the case of unsafe-writability, this method is
+                // unsafe and the caller themselves must provide the
+                // justification.
+                #[allow(unused_unsafe)]
+                unsafe { self.write_impl(value) }
+                ret
+            }
+        }
+
+        impl<Layout, R, Io> Value<Layout, (R, $write_kind), Io>
+        where
+            Io: WriteHandle,
+            Layout: LayoutOver<<Io as IoHandle>::Base>,
+            (R, $write_kind): Writable,
+        {
+            /// Writes the underlying value back to the register.
+            $($(#[$safety_doc])*)?
+            #[inline]
+            pub $($unsafe)? fn write(&self) {
+                // Safety: In the case of unsafe-writability, this method is
+                // unsafe and the caller themselves must provide the
+                // justification.
+                #[allow(unused_unsafe)]
+                unsafe { self.register.write(self.value) }
+            }
+        }
+    };
+}
+
+impl_writable!(SafeWrite);
+impl_writable!(UnsafeWrite, unsafe);
