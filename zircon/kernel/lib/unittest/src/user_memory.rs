80b83a80ad2b3da1fd63a62ca4b192656f6fbf43
Joshua Seaton <joshuaseaton@google.com>
2026-07-17 16:04:58 -0700

    [kernel][unittest][rust] Add UserMemory support
    This was a feature of the C++ library, and it was an oversight to have
    not included it on the Rust side previously.
    
    For simplicity, we define it through FFI for now.
    
    Bug: 517130174
    Change-Id: I22a36948f222fbb00b4a95c3e11c0128df0c701d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1713610
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>

---
+++ zircon/kernel/lib/unittest/src/user_memory.rs
@@ -0,0 +1,92 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use core::ffi::c_void;
+use core::ptr::NonNull;
+use zx_status::Status;
+
+// Opaque type for C++ UserMemory
+#[repr(C)]
+struct RawUserMemory {
+    _facade: zr::OpaqueFacade,
+}
+
+unsafe extern "C" {
+    fn unittest_user_memory_create(size: usize) -> *mut RawUserMemory;
+    fn unittest_user_memory_destroy(mem: *mut RawUserMemory);
+    fn unittest_user_memory_get_base(mem: *const RawUserMemory) -> usize;
+    fn unittest_user_memory_commit_and_map(mem: *mut RawUserMemory, size: usize) -> i32;
+    fn unittest_user_memory_vmo_write(
+        mem: *mut RawUserMemory,
+        ptr: *const c_void,
+        offset: u64,
+        len: u64,
+    ) -> i32;
+    fn unittest_user_memory_vmo_read(
+        mem: *mut RawUserMemory,
+        ptr: *mut c_void,
+        offset: u64,
+        len: u64,
+    ) -> i32;
+}
+
+/// UserMemory facilitates testing code that requires user memory.
+///
+/// Example:
+///    let mut mem = UserMemory::create(size).unwrap();
+///    mem.commit_and_map(size).unwrap();
+///    let out_ptr = UserOutPtr::<u32>::new(mem.base() as *mut u32);
+///    out_ptr.write(value).unwrap();
+pub struct UserMemory {
+    raw: NonNull<RawUserMemory>,
+}
+
+impl UserMemory {
+    pub fn create(size: usize) -> Option<Self> {
+        let raw = unsafe { unittest_user_memory_create(size) };
+        NonNull::new(raw).map(|raw| Self { raw })
+    }
+
+    pub fn base(&self) -> usize {
+        unsafe { unittest_user_memory_get_base(self.raw.as_ptr()) }
+    }
+
+    /// Ensures the mapping is committed and mapped such that usages will cause no faults.
+    pub fn commit_and_map(&mut self, size: usize) -> Result<(), Status> {
+        let status = unsafe { unittest_user_memory_commit_and_map(self.raw.as_ptr(), size) };
+        Status::ok(status)
+    }
+
+    /// Write to the underlying VMO directly, bypassing the mapping.
+    pub fn vmo_write(&mut self, data: &[u8], offset: u64) -> Result<(), Status> {
+        let status = unsafe {
+            unittest_user_memory_vmo_write(
+                self.raw.as_ptr(),
+                data.as_ptr() as *const c_void,
+                offset,
+                data.len() as u64,
+            )
+        };
+        Status::ok(status)
+    }
+
+    /// Read from the underlying VMO directly, bypassing the mapping.
+    pub fn vmo_read(&mut self, data: &mut [u8], offset: u64) -> Result<(), Status> {
+        let status = unsafe {
+            unittest_user_memory_vmo_read(
+                self.raw.as_ptr(),
+                data.as_mut_ptr() as *mut c_void,
+                offset,
+                data.len() as u64,
+            )
+        };
+        Status::ok(status)
+    }
+}
+
+impl Drop for UserMemory {
+    fn drop(&mut self) {
+        unsafe { unittest_user_memory_destroy(self.raw.as_ptr()) };
+    }
+}
