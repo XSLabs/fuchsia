70519993671369b6dbfdb0783084c317d698de3f
Roland McGrath <mcgrathr@google.com>
2026-07-23 14:45:59 -0700

    [userboot] Add gtest fixture to testing library
    This breaks out the test fixture used in the library's own tests
    into a general form to be reused in a gtest-based test components
    for particular collections of userboot programs.
    
    Bug: 521547950
    Change-Id: Ib857ce64c86fecdb81c60441415571457a53aab4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1702454
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Abdulla Kamar <abdulla@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- src/bringup/lib/userboot/unittests.cc
+++ src/bringup/lib/userboot/unittests.cc
@@ -2,82 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/elfldltl/testing/test-pipe-reader.h>
-#include <lib/userboot/testing/launcher.h>
+#include <lib/userboot/testing/fixture.h>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace {
 
-class UserbootTests : public ::testing::Test {
- protected:
-  static void SetUpTestSuite() {
-    auto launcher = userboot::testing::Launcher::Create();
-    ASSERT_TRUE(launcher.is_ok());
-    launcher_ = *std::move(launcher);
-  }
+using ::testing::HasSubstr;
 
-  static void TearDownTestSuite() { launcher_ = {}; }
-
-  static auto& launcher() { return launcher_; }
-
-  void SetUp() override {
-    ASSERT_FALSE(job_);
-    ASSERT_NO_FATAL_FAILURE(job_.Init());
-    ASSERT_FALSE(log_fd_);
-    ASSERT_NO_FATAL_FAILURE(log_.Init(log_fd_));
-  }
-
-  fbl::unique_fd TakeLogFd() {
-    EXPECT_TRUE(log_fd_);
-    return std::exchange(log_fd_, {});
-  }
-
-  std::string FinishLog() { return std::move(log_).Finish(); }
-
-  zx::result<zx::process> Launch(zx::vmo vmo, std::vector<zx::handle> handles) {
-    return launcher().Launch(job_.Get(), std::move(vmo), TakeLogFd(), std::move(handles));
-  }
-
-  zx::result<zx::process> Launch(const char* file, std::vector<zx::handle> handles) {
-    auto vmo = userboot::testing::GetExecutable(file);
-    if (vmo.is_error()) {
-      return vmo.take_error();
-    }
-    return launcher().Launch(job_.Get(), *std::move(vmo), TakeLogFd(), std::move(handles));
-  }
-
- private:
-  inline static userboot::testing::Launcher launcher_;
-
-  userboot::testing::TestJob job_;
-  elfldltl::testing::TestPipeReader log_;
-  fbl::unique_fd log_fd_;
-};
+using UserbootTests = userboot::testing::Fixture;
 
 TEST_F(UserbootTests, BasicLaunch) {
-  auto process = Launch("/pkg/test/userboot-lib-static-pie-test", {});
-  ASSERT_TRUE(process.is_ok());
+  ASSERT_NO_FATAL_FAILURE(Launch("/pkg/test/userboot-lib-static-pie-test", {}));
 
-  auto result = userboot::testing::WaitForTermination(process->borrow());
+  auto result = Wait();
   ASSERT_TRUE(result.is_ok());
   EXPECT_EQ(*result, 0);
 
   std::string log = FinishLog();
-  EXPECT_THAT(log, ::testing::HasSubstr("Hello from userland!")) << log;
+  EXPECT_THAT(log, HasSubstr("Hello from userland!")) << log;
 }
 
 TEST_F(UserbootTests, BasicRustLaunch) {
-  auto process = Launch("/pkg/bin/userboot-lib-rust-static-pie-test", {});
-  ASSERT_TRUE(process.is_ok());
+  ASSERT_NO_FATAL_FAILURE(Launch("/pkg/bin/userboot-lib-rust-static-pie-test", {}));
 
-  auto result = userboot::testing::WaitForTermination(process->borrow());
+  auto result = Wait();
   ASSERT_TRUE(result.is_ok());
   EXPECT_EQ(*result, 0);
 
   std::string log = FinishLog();
-  EXPECT_THAT(log, ::testing::HasSubstr("Hello from Rust userland!")) << log;
+  EXPECT_THAT(log, HasSubstr("Hello from Rust userland!")) << log;
 }
 
 }  // namespace
