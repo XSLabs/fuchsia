9097efe2617da81e3a12fc941cc1f050927fbec8
Joshua Seaton <joshuaseaton@google.com>
2026-07-23 17:09:04 -0700

    [regio] Fix no_std attribute
    We had `#![cfg_attr(no_std, not(test))]` instead of
    `#![cfg_attr(not(test), no_std)]`!
    
    Bug: 525077555
    Change-Id: Ie258dc22b067245438d188dc4a9bfa0ca252b507
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1721810
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>

--- src/lib/regio/src/lib.rs
+++ src/lib/regio/src/lib.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![cfg_attr(no_std, not(test))]
+#![cfg_attr(not(test), no_std)]
 
 mod mmio;
 pub mod traits;
