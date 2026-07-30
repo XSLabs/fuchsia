37aa1d100dd4bd1ef72f2f605464c1cfd6497405
Ethan Williams <ethanws@google.com>
2026-07-30 14:20:33 -0700

    [kernel][rust] Use bindgen for VmObject
    Use bindgen to generate bindings for VmObject and existing associated
    functions.
    
    Bug: 539998429
    Change-Id: Id58703b2de4f7620704bb29f1b1153a143777a63
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1729693
    Reviewed-by: Adrian Danis <adanis@google.com>
    Commit-Queue: Ethan Williams <ethanws@google.com>
    Reviewed-by: Rasha Eqbal <rashaeqbal@google.com>

--- zircon/kernel/vm/vm_object.rs
+++ zircon/kernel/vm/vm_object.rs
@@ -4,17 +4,61 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
-unsafe extern "C" {
-    fn cpp_vm_object_get_ref_counted(vmo: *const VmObject) -> *mut fbl::RefCounted;
-    fn cpp_vm_object_free(vmo: *mut VmObject);
+use core::marker::{PhantomData, PhantomPinned};
+use core::ptr::NonNull;
+use fbl::{HasRefCount, Recyclable, RefPtr};
+use kalloc::AllocError;
+use vm_object_bindings as bindings;
+
+/// The base vm object that holds a range of bytes of data
+///
+/// Can be created without mapping and used as a container of data, or mappable
+/// into an address space via VmAddressRegion::CreateVmMapping
+#[repr(C)]
+pub struct VmObject {
+    raw: bindings::VmObject,
+    phantom: PhantomData<PhantomPinned>,
 }
 
-fbl::impl_opaque_ref_counted_facade!(
-    /// The base vm object that holds a range of bytes of data
+impl VmObject {
+    /// Domain-specific conversion: returns raw pointer for `VmObject`.
+    pub fn as_raw(&self) -> *mut bindings::VmObject {
+        core::ptr::from_ref(&self.raw).cast_mut()
+    }
+
+    /// Domain-specific conversion: constructs a `RefPtr` from an exported pointer.
     ///
-    /// Can be created without mapping and used as a container of data, or mappable
-    /// into an address space via VmAddressRegion::CreateVmMapping
-    pub struct VmObject,
-    cpp_vm_object_free,
-    cpp_vm_object_get_ref_counted,
-);
+    /// # Safety
+    ///
+    /// `ptr` must be a valid raw `VmObject` pointer exported from C++.
+    pub unsafe fn from_raw(ptr: *mut bindings::VmObject) -> Option<RefPtr<Self>> {
+        unsafe { RefPtr::try_from_raw(ptr.cast::<Self>()) }
+    }
+
+    /// Returns a pointer to the underlying `VmObject` structure.
+    ///
+    /// This method is helpful when you don't have a reference to the `VmObject`. If you do, then
+    /// use `VmObject::as_raw` instead.
+    pub fn cast_raw(ptr: *mut VmObject) -> *mut bindings::VmObject {
+        ptr.cast()
+    }
+}
+
+impl HasRefCount for VmObject {
+    fn ref_count(&self) -> &fbl::RefCounted {
+        let raw = unsafe { bindings::cpp_vm_object_get_ref_counted(self.as_raw()) };
+        unsafe { &*(raw.cast::<fbl::RefCounted>()) }
+    }
+}
+
+unsafe impl Recyclable for VmObject {
+    unsafe fn recycle(ptr: NonNull<Self>) {
+        unsafe {
+            bindings::cpp_vm_object_free(VmObject::cast_raw(ptr.as_ptr()));
+        }
+    }
+
+    fn allocate(_value: Self) -> Result<NonNull<Self>, AllocError> {
+        Err(AllocError)
+    }
+}
