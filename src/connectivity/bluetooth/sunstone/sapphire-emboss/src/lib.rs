17dcb7cb44eb9e559aa1a79d4def4003812ca447
Rasha Eqbal <rashaeqbal@google.com>
2026-07-31 12:54:15 -0700

    Revert "[bt][sunstone] Add all emboss definitions"
    This reverts commit fcde7f295246337af41cc0e32920ee0f4d56349d.
    
    Reason for revert: Broke core.x64-asan-bazel_consistency builder in CI
    
    Failure Link: https://ci.chromium.org/ui/p/turquoise/builders/global.ci/core.x64-asan-bazel_consistency/b8674805111077399121/overview
    
    Original change's description:
    > [bt][sunstone] Add all emboss definitions
    >
    > Fixed: 539540117
    > Change-Id: I58827c006aff22fef8c71225176809a86a6a6964
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1726649
    > Reviewed-by: Ben Lawson <benlawson@google.com>
    > Commit-Queue: Adam Snaider <asnaider@google.com>
    > Fuchsia-Auto-Submit: Adam Snaider <asnaider@google.com>
    
    Bug: 539540117
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If80bb931b62f37af181691cdaef31afecc4cfffb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1733611
    Commit-Queue: Rasha Eqbal <rashaeqbal@google.com>
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>

--- src/connectivity/bluetooth/sunstone/sapphire-emboss/src/lib.rs
+++ src/connectivity/bluetooth/sunstone/sapphire-emboss/src/lib.rs
@@ -2,22 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-pub use pw_bluetooth_hci_commands_emb as hci_commands;
-pub use pw_bluetooth_hci_common_emb as hci_common;
-pub use pw_bluetooth_hci_h4_emb as hci_h4;
-pub use pw_bluetooth_l2cap_frames_emb as l2cap_frames;
+pub use example_emb as example;
+pub use example2_emb as example2;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use hci_commands::ResetCommand;
-    use hci_common::OpCode;
+    use example::ExampleHeader;
+    use example2::Example2Packet;
 
     #[test]
-    fn test_reset_command() {
-        let buffer = [0x03u8, 0x0cu8, 0x00u8];
-        let view = ResetCommand::new(&buffer[..]);
-        assert_eq!(view.header().opcode().try_read().unwrap(), OpCode::RESET);
-        assert_eq!(view.header().parameter_total_size().try_read().unwrap(), 0);
+    fn test_example_header() {
+        let buffer = [0x42u8, 0x10u8];
+        let view = ExampleHeader::new(&buffer[..]);
+        assert_eq!(view.opcode().try_read().unwrap(), 0x42);
+        assert_eq!(view.length().try_read().unwrap(), 0x10);
+    }
+
+    #[test]
+    fn test_example2_packet_with_imported_example() {
+        let buffer = [0x42u8, 0x10u8, 0x05u8, 0x00u8];
+        let view = Example2Packet::new(&buffer[..]);
+        assert_eq!(view.header().opcode().try_read().unwrap(), 0x42);
+        assert_eq!(view.header().length().try_read().unwrap(), 0x10);
+        assert_eq!(view.payload_id().try_read().unwrap(), 5);
     }
 }
