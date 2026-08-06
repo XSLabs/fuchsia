577b837ec1484749f9c076d68aee5063ff2d4b60
Jason Yi <jsonyi@google.com>
2026-08-06 12:00:22 -0700

    [bt][sunstone-emboss] Export ATT Emboss Target
    Add the pw_bluetooth_att_emb GN target to sapphire-emboss and re-export
    it as sapphire_emboss::att for downstream Sunstone ATT usage.
    
    Test: fx test sapphire-emboss_test
    Change-Id: Ie0556ed37d74a314cf2d661cafaba9846a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1735492
    Reviewed-by: Adam Snaider <asnaider@google.com>
    Commit-Queue: Jason Yi <jsonyi@google.com>

--- src/connectivity/bluetooth/sunstone/sapphire-emboss/src/lib.rs
+++ src/connectivity/bluetooth/sunstone/sapphire-emboss/src/lib.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+pub use pw_bluetooth_att_emb as att;
 pub use pw_bluetooth_hci_commands_emb as hci_commands;
 pub use pw_bluetooth_hci_common_emb as hci_common;
 pub use pw_bluetooth_hci_h4_emb as hci_h4;
@@ -10,6 +11,7 @@ pub use pw_bluetooth_l2cap_frames_emb as l2cap_frames;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use att::{AttErrorRsp, AttErrorRspMut, AttOpcode, ErrorCode};
     use hci_commands::ResetCommand;
     use hci_common::OpCode;
 
@@ -19,5 +21,24 @@ mod tests {
         let view = ResetCommand::new(&buffer[..]);
         assert_eq!(view.header().opcode().try_read().unwrap(), OpCode::RESET);
         assert_eq!(view.header().parameter_total_size().try_read().unwrap(), 0);
+    }
+
+    #[test]
+    fn test_error_rsp() {
+        let mut buffer = [0u8; 5];
+        let mut view = AttErrorRspMut::new(&mut buffer[..]);
+        view.attribute_opcode().try_write(AttOpcode::ATT_ERROR_RSP).unwrap();
+        view.request_opcode_in_error().try_write(AttOpcode::ATT_READ_REQ).unwrap();
+        view.attribute_handle().try_write(0x1234).unwrap();
+        view.error_code().try_write(ErrorCode::READ_NOT_PERMITTED).unwrap();
+
+        let read_view = AttErrorRsp::new(&buffer[..]);
+        assert_eq!(read_view.attribute_opcode().try_read().unwrap(), AttOpcode::ATT_ERROR_RSP);
+        assert_eq!(
+            read_view.request_opcode_in_error().try_read().unwrap(),
+            AttOpcode::ATT_READ_REQ
+        );
+        assert_eq!(read_view.attribute_handle().try_read().unwrap(), 0x1234);
+        assert_eq!(read_view.error_code().try_read().unwrap(), ErrorCode::READ_NOT_PERMITTED);
     }
 }
